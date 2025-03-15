322d3abf82804c847787c8990c4d8724c9fe704a
Tmrcdsk <129770406+Tmrcdsk@users.noreply.github.com>
2025-03-15 16:08:06 +0800

    练习：利用framebuffers创建一个后视镜

--- LearnOpenGL/res/shaders/framebuffersScreenFragment.glsl
+++ LearnOpenGL/res/shaders/framebuffersScreenFragment.glsl
@@ -5,36 +5,8 @@ in vec2 vTexCoords;
 
 uniform sampler2D uScreenTexture;
 
-const float offset = 1.0 / 300.0;
-
 void main()
 {
-    vec2 offsets[9] = vec2[](
-        vec2(-offset,  offset), // 左上
-        vec2( 0.0f,    offset), // 正上
-        vec2( offset,  offset), // 右上
-        vec2(-offset,  0.0f),   // 左
-        vec2( 0.0f,    0.0f),   // 中
-        vec2( offset,  0.0f),   // 右
-        vec2(-offset, -offset), // 左下
-        vec2( 0.0f,   -offset), // 正下
-        vec2( offset, -offset)  // 右下
-    );
-
-    float kernel[9] = float[](
-        1,  1, 1,
-        1, -8, 1,
-        1,  1, 1
-    );
-
-    vec3 sampleTex[9];
-    for(int i = 0; i < 9; i++)
-    {
-        sampleTex[i] = vec3(texture(uScreenTexture, vTexCoords.st + offsets[i]));
-    }
-    vec3 col = vec3(0.0);
-    for(int i = 0; i < 9; i++)
-        col += sampleTex[i] * kernel[i];
-
+    vec3 col = texture(uScreenTexture, vTexCoords).rgb;
     FragColor = vec4(col, 1.0);
 } 
--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -138,13 +138,13 @@ int main()
 	};
 	float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
 		// positions   // texCoords
-		-1.0f,  1.0f,  0.0f, 1.0f,
-		-1.0f, -1.0f,  0.0f, 0.0f,
-		 1.0f, -1.0f,  1.0f, 0.0f,
+		-0.3f,  1.0f,  0.0f, 1.0f,
+		-0.3f,  0.7f,  0.0f, 0.0f,
+		 0.3f,  0.7f,  1.0f, 0.0f,
 
-		-1.0f,  1.0f,  0.0f, 1.0f,
-		 1.0f, -1.0f,  1.0f, 0.0f,
-		 1.0f,  1.0f,  1.0f, 1.0f
+		-0.3f,  1.0f,  0.0f, 1.0f,
+		 0.3f,  0.7f,  1.0f, 0.0f,
+		 0.3f,  1.0f,  1.0f, 1.0f
 	};
 	// cube VAO
 	unsigned int cubeVAO, cubeVBO;
@@ -243,9 +243,12 @@ int main()
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		// render
-		// ------
+		// first render pass: mirror texture.
+		// bind to framebuffer and draw to color texture as we normally 
+		// would, but with the view camera reversed.
 		// bind to framebuffer and draw scene as we normally would to color texture 
+		// 将镜面场景渲染到帧缓冲
+		// ------------------------------------------------------------------------
 		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 		glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
 
@@ -255,7 +258,11 @@ int main()
 
 		shader.Bind();
 		glm::mat4 model = glm::mat4(1.0f);
+		camera.Yaw += 180.0f; // rotate the camera's yaw 180 degrees around
+		camera.ProcessMouseMovement(0, 0, false); // call this to make sure it updates its camera vectors, note that we disable pitch constrains for this specific case (otherwise we can't reverse camera's pitch values)
 		glm::mat4 view = camera.GetViewMatrix();
+		camera.Yaw -= 180.0f; // reset it back to its original orientation
+		camera.ProcessMouseMovement(0, 0, true);
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)Width / Height, 0.1f, 100.0f);
 		shader.SetUniformMat4("uView", view);
 		shader.SetUniformMat4("uProjection", projection);
@@ -278,16 +285,45 @@ int main()
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
 
-		// now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
+
+		// second render pass: draw as normal
+		// 正常渲染
+		// ----------------------------------
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		glClearColor(0.6627f, 0.6588f, 0.5961f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		model = glm::mat4(1.0f);
+		view = camera.GetViewMatrix();
+		shader.SetUniformMat4("uView", view);
+
+		// cubes
+		glBindVertexArray(cubeVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		shader.SetUniformMat4("uModel", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		shader.SetUniformMat4("uModel", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		// floor
+		glBindVertexArray(planeVAO);
+		glBindTexture(GL_TEXTURE_2D, floorTexture);
+		model = glm::mat4(1.0f);
+		shader.SetUniformMat4("uModel", model);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+		// now draw the mirror quad with screen texture
+		// --------------------------------------------
 		glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
-		// clear all relevant buffers
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
 
 		screenShader.Bind();
 		glBindVertexArray(quadVAO);
-		glBindTexture(GL_TEXTURE_2D, textureColorbuffer); // use the color attachment texture as the texture of the quad plane
+		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);	// use the color attachment texture as the texture of the quad plane
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
